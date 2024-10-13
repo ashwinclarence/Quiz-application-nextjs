05cc92595b219ff33fd544819ad42f81c140d4c4
ASHWIN <meashwinclarence@gmail.com>
2024-10-13 13:46:43 +0530

    fea: store provider added

---
+++ src/app/StoreProvider.tsx
@@ -0,0 +1,11 @@
+import { Provider } from "react-redux";
+import { AppStore, store } from "./lib/store";
+import { useRef } from "react";
+
+export default function StoreProvider({children}: {children: React.ReactNode}) {
+  const storeRef = useRef<AppStore | null>(null);
+  if (!storeRef.current) {
+    storeRef.current = store();
+  }
+  return <Provider store={storeRef.current}>{children}</Provider>;
+}
